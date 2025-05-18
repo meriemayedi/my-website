2ee43a2f67294a3b7d1bde1dbab44f932ee7d1a0
meriem <maryemayadi244@gmail.com>
2025-05-18 06:32:28 +0100

    Added button and styling

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; // This links your CSS
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App!</h1>
+      <h1>My First React App</h1>
+      <p>Start editing here!</p>
+
+    </div>
+  );
+}
+
+export default App;
