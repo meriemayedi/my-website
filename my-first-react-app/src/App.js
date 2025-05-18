bdf9ba1fc3c36a05aae7e7add303e93b9e87389b
meriem <maryemayadi244@gmail.com>
2025-05-18 11:22:42 +0100

    W9: React Data fetching

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,14 +1,38 @@
-import * as React from "react";
-function getTitle(title) {
-  return title;
-}
+import * as React from 'react';
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  children,
+}) => (
+  <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
+    />
+  </>
+);
+
 const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      <Item
+        key={item.objectID}
+        item={item}
+        onRemoveItem={onRemoveItem}
+      />
     ))}
   </ul>
 );
+
 const Item = ({ item, onRemoveItem }) => (
   <li>
     <span>
@@ -24,81 +48,81 @@ const Item = ({ item, onRemoveItem }) => (
     </span>
   </li>
 );
-const Search = ({ search, onSearch }) => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
-  </div>
-);
 
 const App = () => {
-  const initialStories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-  const [stories, setStories] = React.useState(initialStories);
+  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+  };
+
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
       (story) => item.objectID !== story.objectID
     );
     setStories(newStories);
   };
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem("search") || "React"
-  );
+
   React.useEffect(() => {
-    localStorage.setItem("search", searchTerm);
-  }, [searchTerm]);
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-  const searchdStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  console.log("App renders");
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setStories(result.hits);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (url !== `${API_ENDPOINT}`) {
+      fetchData();
+    }
+  }, [url]);
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
+      
       <InputWithLabel
         id="search"
         value={searchTerm}
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         <strong>Search:</strong>
       </InputWithLabel>
+      
+      <button
+        type="button"
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}
+      >
+        Submit
+      </button>
+      
       <hr />
-      <List list={searchdStories} onRemoveItem={handleRemoveStory} />
+      
+      {isError && <p>Something went wrong ...</p>}
+      
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={stories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
-
-const InputWithLabel = ({
-  id,
-  value,
-  type = "text",
-  onInputChange,
-  children,
-}) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input id={id} type={type} value={value} onChange={onInputChange} />
-  </>
-);
 
 export default App;
