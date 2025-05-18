1a761f8616e92a5b316d504640f3e70188d7a4f3
meriem <maryemayadi244@gmail.com>
2025-05-18 08:19:12 +0100

    Updated App.js with new feature

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,38 +1,43 @@
-// 1. Import statements (ALWAYS AT THE TOP)
-import './App.css';
+import React from 'react';
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-// 2. Components (before exports)
-function Header() {
-  return <h2>My Header</h2>;
-}
-
-// 3. Main App component
 function App() {
-  // Variables go here
-  
-  const appTitle = "My React App";
-  const buttonText = "Click Me";
-
-  // JSX goes in the return statement
   return (
-    <div className="app">
-      {/* Header component */}
-      <Header />
-      
-      {/* Main content */}
-      <h1>Welcome to {appTitle}!</h1>
-      <label htmlFor="search">Search: </label> {/* Label for input */}
-    <input id="search" type="text" /> {/* Input field */}
-      
-      <button onClick={() => alert("Hello!")}>
-        {buttonText}
-      </button>
-      
-      
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a> | 
+            Author: {item.author} | 
+            Comments: {item.num_comments} | 
+            Points: {item.points}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-// 4. Exports (ALWAYS LAST)
 export default App;
